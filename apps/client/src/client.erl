8a6b9d7c2181408969d283206fe465a02a5c66e2
Dmitriy <dmitriy7smirnov@gmail.com>
2019-08-15 21:24:48 +0300

    Maks is changed client server for authentication and rebar.config corrected
    Signed-off-by: Dmitriy <dmitriy7smirnov@gmail.com>

--- apps/client/src/client.erl
+++ apps/client/src/client.erl
@@ -10,7 +10,8 @@
 -export([
     start/0,
     start/2,
-    send/2,
+    login/3,
+    send_text/2,
     stop/1,
     client/2
 ]).
@@ -22,8 +23,12 @@ start() ->
 start(Host, Port) ->
     spawn(?MODULE, client, [Host, Port]).
 
-send(Pid, Msg) ->
-    Pid ! {send, Msg},
+login(Pid, Username, Password) ->
+    Pid ! {login, Username, Password},
+    ok.
+
+send_text(Pid, Text) ->
+    Pid ! {text, Text},
     ok.
 
 stop(Pid) ->
@@ -31,29 +36,38 @@ stop(Pid) ->
     ok.
 
 client(Host, Port) ->
-    io:format("Client ~p connects to ~p:~p~n", [self(), Host, Port]),
+    io:format("CLIENT: pid = ~p connects to ~s:~p~n", [self(), Host, Port]),
     {ok, Socket} = gen_tcp:connect(Host, Port, [binary, {active, true}, {packet, raw}]),
     loop(Socket).
 
 loop(Socket) ->
     receive
-        {send, Msg} ->
-            io:format("Client ~p send ~p~n", [self(), Msg]),
-            Size = byte_size(Msg),
-            Header = <<Size:16/integer>>,
-            gen_tcp:send(Socket, <<Header/binary, Msg/binary>>),
+        {login, Username, Password} ->
+            PackedUsername = proto:pack(string, Username),
+            PackedPassword = proto:pack(string, Password),
+            send(Socket, <<PackedUsername/binary, PackedPassword/binary>>),
+            io:format("CLIENT: trying to login as \"~s\" with password \"~s\"~n", [Username, Password]),
+            loop(Socket);
+        {text, Text} ->
+            send(Socket, proto:pack(string, Text)),
+            io:format("CLIENT: sending text ~s~n", [Text]),
             loop(Socket);
         {tcp, Socket, Msg} ->
-            io:format("Client ~p got message: ~p~n", [self(), Msg]),
+            Text = recv(Msg),
+            io:format("CLIENT: got message: ~s~n", [Text]),
             loop(Socket);
         stop ->
-            io:format("Client ~p closes connection and stops~n", [self()]),
+            io:format("CLIENT: close connection and stop~n", []),
             gen_tcp:close(Socket)
-    after 200 ->
-            loop(Socket)
+    after
+        200 -> loop(Socket)
     end.
 
+recv(<<DataSize:16/integer, Data:DataSize/binary>>) ->
+    {Text, _} = proto:parse(string, Data),
+    Text.
 
-
-
-
+send(Socket, Data) ->
+    DataSize = byte_size(Data),
+    Packet = <<DataSize:16/integer, Data/binary>>,
+    gen_tcp:send(Socket, Packet).
--- apps/server/src/server.erl
+++ apps/server/src/server.erl
@@ -4,25 +4,30 @@
 -module(server).
 
 %% Include files
+-record(player_db_entry, {
+    id,
+    username,
+    password
+}).
+
+-record(player, {
+    socket,
+    id,
+    username
+}).
 
 %% Exported Functions
-
-
 -export([
     start/0,
     start/1,
     server/1,
-    accept/2,
-    auth_map/0
+    accept/2
 ]).
-
-
 
 start() ->
     start(1234),
     receive
-        _Msg ->
-            77
+        _Msg -> 77
     end.
 
 start(Port) ->
@@ -30,61 +35,79 @@ start(Port) ->
     ok.
 
 server(Port) ->
-    io:format("start server at port ~p~n", [Port]),
+    io:format("SERVER: listening on port ~p...~n", [Port]),
     {ok, ListenSocket} = gen_tcp:listen(Port, [binary, {active, false}, {packet, raw}]),
     [spawn(?MODULE, accept, [Id, ListenSocket]) || Id <- lists:seq(1, 5)],
     timer:sleep(infinity),
     ok.
 
 accept(Id, ListenSocket) ->
-    io:format("Socket #~p wait for client~n", [Id]),
+    % io:format("SERVER: socket #~p waiting for client~n", [Id]),
     {ok, Socket} = gen_tcp:accept(ListenSocket),
-    io:format("Socket #~p, session started, input login:~n", [Id]),
-    AuthState = auth_map(),
-    handle_connection(Id, ListenSocket, Socket, AuthState).
+    % io:format("SERVER: socket #~p, session started~n", [Id]),
+    handle_connection(#player{socket = Socket}),
+    % io:format("SERVER: socket #~p, session closed~n", [Id]),
+    accept(Id, ListenSocket).
 
-
-handle_connection(Id, ListenSocket, Socket, AuthState) ->
+recv(Socket) ->
     case gen_tcp:recv(Socket, 2) of
-        {ok, Header} -> <<Size:16/integer>> = Header,
-                        {ok, Msg} = gen_tcp:recv(Socket, Size),
-                        io:format("Socket #~p got message: ~p~n", [Id, Msg]),
-                        Msg1 = binary_to_list(Msg),
-                        AuthState1 = case maps:find(islogined, AuthState) of
-                            {ok, false} ->
-                                case maps:find(logincorrect, AuthState) of 
-                                    {ok, false} -> case maps:find(login, AuthState) of
-                                                       {ok, Msg1} -> gen_tcp:send(Socket, <<"input password">>),
-                                                       maps:update(logincorrect, true, AuthState);
-                                                       _NotCorrect -> gen_tcp:send(Socket, <<"try login again:">>),
-                                                        AuthState
-                                                   end;
-                                    {ok, true} -> case maps:find(password, AuthState) of
-                                                       {ok, Msg1} -> gen_tcp:send(Socket, <<"you are logined">>),
-                                                       AuthStateTemp = maps:update(passwordcorrect, true, AuthState),
-                                                       maps:update(islogined, true, AuthStateTemp);
-                                                       _NotCorrect -> gen_tcp:send(Socket, <<"try password again:">>),
-                                                       AuthState
-                                                   end         
-                                end;
-                             {ok, true} -> gen_tcp:send(Socket, <<"You are logged yet">>),
-                                           AuthState;
-                                    _Any ->
-                                           AuthState
-                         end,   
-                        handle_connection(Id, ListenSocket, Socket, AuthState1);
-        {error, closed} ->
-            io:format("Socket #~p, session closed ~n", [Id]),
-            accept(Id, ListenSocket)
+        {ok, <<DataSize:16/integer>>} ->
+            case gen_tcp:recv(Socket, DataSize) of
+                {ok, Data} -> {ok, Data};
+                {error, Reason} -> {error, Reason}
+            end;
+        {error, Reason} ->
+            {error, Reason}
     end.
 
+send(Socket, Data) ->
+    DataSize = byte_size(Data),
+    Packet = <<DataSize:16/integer, Data/binary>>,
+    gen_tcp:send(Socket, Packet).
 
-auth_map() -> #{
-        login => "Maksik",
-        logincorrect => false,
-        password => "kleviy",
-        passwordcorrect => false,
-        islogined => false
-    
+handle_connection(#player{socket = Socket, id = Id} = PlayerState) ->
+    case recv(Socket) of
+        % Not logged in
+        {ok, Data} when Id =:= undefined ->
+            {Username, Rest} = proto:parse(string, Data),
+            {Password, _Rest1} = proto:parse(string, Rest),
+            case authenticate(Username, Password) of
+                {ok, #player_db_entry{id = UserId}} ->
+                    io:format("SERVER: user [#~B] ~s logged in successfully~n", [UserId, Username]),
+                    Reply = proto:pack(string, <<"OK">>),
+                    send(Socket, Reply),
+                    handle_connection(PlayerState#player{id = UserId, username = Username});
+                {error, Reason} ->
+                    io:format("SERVER: user ~s login failed. Reason: ~p~n", [Username, Reason]),
+                    Reply = proto:pack(string, atom_to_binary(Reason, utf8)),
+                    send(Socket, Reply)
+            end;
+
+        % Logged in
+        {ok, Data} ->
+            {Text, _} = proto:parse(string, Data),
+            io:format("SERVER: message from client: ~s~n", [Text]),
+            Reply = proto:pack(string, <<"RECV OK">>),
+            send(Socket, Reply),
+            handle_connection(PlayerState);
+
+        % Error
+        {error, Reason} ->
+            {error, Reason}
+    end.
+
+players() ->
+    #{
+        <<"dima">> => #player_db_entry{
+            id = 1,
+            username = <<"dima">>,
+            password = <<"123">>
+        }
     }.
 
+authenticate(Username, Password) ->
+    case maps:get(Username, players(), undefined) of
+        #player_db_entry{username = Username, password = Password} = E -> {ok, E};
+        #player_db_entry{} -> {error, invalid_password};
+        undefined -> {error, invalid_username}
+    end.
